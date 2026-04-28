8ec16d5895a37c6ca38a906ba53da6b399463b97
Dylan <dverge480@gmail.com>
2026-04-28 02:30:42 -0400

    final

---
+++ final/challenge1.bash
@@ -0,0 +1,28 @@
+echo "Checking for IOC.html in the current directory"
+
+# Verification
+if [[ ! -f "IOC.html" ]]; then
+  echo "Error: IOC.html not found in the current directory."
+  echo "Please make sure the file is saved here before running this script."
+  exit 1
+fi
+
+# File Exists
+xmlstarlet sel -H -t -m "//table/tr[position()>1]" \
+ -v "td[1]" -o "  " \
+ -v "td[3]" -n IOC.html > IOC.txt
+echo ""
+
+# Output
+if [[ -f "IOC.txt" ]]; then
+  echo "Success: IOC.txt has been created."
+else
+  echo "Warning: IOC.txt was not created."
+  exit 1
+fi
+
+echo "Done."
+
+echo "" 
+
+cat IOC.txt
---
+++ final/challenge2.bash
@@ -0,0 +1,22 @@
+echo "Enter the name of the file to be scanned:"
+read logfile
+
+echo ""
+
+echo "Enter the IOC file:"
+read iocfile
+# In our case, iocfile should be IOC.txt
+
+# Clearing report file
+> report.txt
+
+
+while IFS= read -r ioc;
+do
+	ioc=$(echo "$ioc" | xargs)
+	[[ -z "$ioc" ]] && continue
+	grep -Fi "$ioc" "$logfile" | awk '{print $1, $4, $7}' >> report.txt
+
+done < "$iocfile"
+
+echo "Scan complete. Results saved to report.txt"
